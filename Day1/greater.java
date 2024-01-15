6a70bfb9dd23d7546176a9dbe07c70091b7a81ca
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2024-01-16 00:31:46 +0530

    Create greater.java

---
+++ Day1/greater.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 2,5,1,3,0 };
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        System.out.println(max);
+    }
+}
