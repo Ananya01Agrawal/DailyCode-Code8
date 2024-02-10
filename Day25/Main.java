e4046c9ec5edf30f20c12123195979a2473752d8
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2024-02-10 12:13:55 +0530

    Create Main.java

---
+++ Day25/Main.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    static int findFloor(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int findCeil(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+        int f = findFloor(arr, n, x);
+        int c = findCeil(arr, n, x);
+        return new int[] {f, c};
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array in sorted order:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.print("Enter the number to find floor and ceil: ");
+        int x = scanner.nextInt();
+        scanner.close();
+
+        int[] ans = getFloorAndCeil(arr, n, x);
+        System.out.println("The floor and ceil are: " + ans[0] + " " + ans[1]);
+    }
+}
